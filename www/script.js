1c18f6d1708fa79e3c9153dd535288f12b28d6c1
ceemeese <cmsalvador92@gmail.com>
2024-12-23 00:12:21 +0100

    feat: Añadir funcionalidad para agregar categorías

---
+++ www/script.js
@@ -0,0 +1,36 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+
+    console.log('Hola script principal')
+
+
+    const datos = {
+        name: 'Nueva Categoria'
+    };
+
+    const postCategory = async(data) => {
+        try {
+            const response = await fetch('http://localhost:3000/categories', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',  
+                },
+                body: JSON.stringify(datos)
+            });
+
+            if(response.status === 200) {
+                const data = await response.json()
+                console.log(data);
+            } else if (response.status === 401) {
+                console.log('Hay un error en la petición')
+            } else {
+            console.log('Hubo un error, revisa parámetros')
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+
+    postCategory(datos);
+
+})
